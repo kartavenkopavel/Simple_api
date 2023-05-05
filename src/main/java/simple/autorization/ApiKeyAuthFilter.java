70480305fe4b2ef417f845b496fa13f56665496c
Pavel Kartavenko <63100397+kartavenkopavel@users.noreply.github.com>
2023-05-05 11:33:18 +0300

    add api key authentication (#1)

---
+++ src/main/java/simple/autorization/ApiKeyAuthFilter.java
@@ -0,0 +1,24 @@
+package simple.autorization;
+
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ApiKeyAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    private final String headerName;
+
+    public ApiKeyAuthFilter(final String headerName) {
+        this.headerName = headerName;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        return request.getHeader(headerName);
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return null;
+    }
+}
---
+++ src/main/java/simple/autorization/SecurityConfig.java
@@ -0,0 +1,52 @@
+package simple.autorization;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Objects;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class SecurityConfig {
+
+    @Value("${app.http.auth-token-header-name}")
+    private String principalRequestHeader;
+
+    @Value("${app.http.auth-token}")
+    private String principalRequestValue;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        ApiKeyAuthFilter filter = new ApiKeyAuthFilter(principalRequestHeader);
+        filter.setAuthenticationManager(
+                authentication -> {
+                    String principal = (String) authentication.getPrincipal();
+                    if (!Objects.equals(principalRequestValue,principal)) {
+                        throw new BadCredentialsException(
+                                "The API Key was not found or not the expected value.");
+                    }
+                    authentication.setAuthenticated(true);
+                    return authentication;
+                });
+        http.antMatcher("/**")
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(filter)
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+
+        return http.build();
+    }
+}
