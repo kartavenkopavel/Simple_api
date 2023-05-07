29985e732e278ab30f90d06cae1c36597dc40837
Kartavenko Pavel <gregg190@gmail.com>
2023-05-07 13:49:00 +0300

    added swagger docs

--- src/main/java/simple/Application.java
+++ src/main/java/simple/Application.java
@@ -1,9 +1,11 @@
 package simple;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
